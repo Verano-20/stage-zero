016cd6503db693febb44f6ba0d0faf9f48933323
Verano-20 <samdawson97@hotmail.co.uk>
2025-10-08 22:54:27 +0100

    fix example tags

--- internal/response/response.go
+++ internal/response/response.go
@@ -2,10 +2,10 @@ package response
 
 type ApiResponse struct {
 	Message string `json:"message" example:"Operation successful"`
-	Data    any    `json:"data" example:"null"`
+	Data    any    `json:"data"`
 }
 
 type ErrorResponse struct {
 	Error   string            `json:"error" example:"An error occurred"`
-	Details map[string]string `json:"details" example:"null"`
+	Details map[string]string `json:"details"`
 }
